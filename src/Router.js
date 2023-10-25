7b006bd54d94e77bd860baa734db464401100c19
Uğur Gümüşhan <ugur.gumushan@icloud.com>
2023-10-25 18:18:33 +0300

    Lint

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BackHandler, Linking } from 'react-native';
-import {ViewPropTypes} from 'deprecated-react-native-prop-types';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 import PropTypes from 'prop-types';
 import NavigationStore from './Store';
 import defaultStore from './defaultStore';
